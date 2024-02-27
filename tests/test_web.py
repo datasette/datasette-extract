4b5a762496e74dad19370ad0ee9b97f0431ae35a
Simon Willison <swillison@gmail.com>
2024-02-27 09:10:46 -0800

    Update test for new URL structure, refs #6

--- tests/test_web.py
+++ tests/test_web.py
@@ -6,6 +6,6 @@ import pytest
 async def test_extract_web():
     ds = Datasette(memory=True)
     ds.add_memory_database("data")
-    response = await ds.client.get("/-/extract/data")
+    response = await ds.client.get("/data/-/extract")
     assert response.status_code == 200
     assert "<h1>Extract data and create a new table in data</h1>" in response.text
