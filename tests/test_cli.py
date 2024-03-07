72a60e1513ca1e8cb32bee9638873971c9f1f1b5
Simon Willison <swillison@gmail.com>
2024-03-07 12:19:27 -0500

    Remove 'datasette extract' CLI skeleton for the moment, refs #13

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-from datasette.cli import cli
-from click.testing import CliRunner
-
-
-def test_extract_command():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["extract", "database", "table"])
-    assert result.exit_code == 0
-    assert result.output == "Will extract to table in database\n"
