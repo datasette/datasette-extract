835f3396a665c7aefc633d044c664d113ef8626b
Simon Willison <swillison@gmail.com>
2025-04-15 21:45:54 -0700

    Maybe this will help tests pass

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 @pytest.fixture(autouse=True)
 def mock_api_key(monkeypatch):
     monkeypatch.setenv("DATASETTE_SECRETS_OPENAI_API_KEY", "mock-api-key")
+    monkeypatch.setenv("OPENAI_API_KEY", "mock-api-key")
 
 
 @pytest.fixture(scope="module")
